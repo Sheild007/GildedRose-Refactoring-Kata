015789781ac203240d7083045be90e03bc5f3860
Usman <usman.muneer720@gmail.com>
2026-09-19 20:57:25 +0500

    refactor: replace nested if/else with individual methods per item type

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,36 +10,41 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > MIN_QUALITY:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+            item.sell_in -= 1
+            if item.name == "Aged Brie":
+                self._update_aged_brie(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self._update_backstage_pass(item)
             else:
-                if item.quality < MAX_QUALITY:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 0:
-                            item.quality = MIN_QUALITY
-                        if item.sell_in < 10:
-                            item.quality = min(MAX_QUALITY, item.quality + 1)
-                        if item.sell_in < 5:
-                            item.quality = min(MAX_QUALITY, item.quality + 2)
-                        else:
-                            item.quality = min(MAX_QUALITY, item.quality + 1)
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > MIN_QUALITY:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = MIN_QUALITY
-                else:
-                    if item.quality < MAX_QUALITY:
-                        item.quality = item.quality + 1
+                self._update_normal(item)
+
+    def _update_normal(self, item):
+        # Degrades 1/day, 2/day after sell_in passes
+        if item.sell_in < 0:
+            item.quality = max(MIN_QUALITY, item.quality - 2)
+        else:
+            item.quality = max(MIN_QUALITY, item.quality - 1)
+
+    def _update_aged_brie(self, item):
+        # Appreciates 1/day, 2/day after sell_in passes
+        if item.sell_in < 0:
+            item.quality = min(MAX_QUALITY, item.quality + 2)
+        else:
+            item.quality = min(MAX_QUALITY, item.quality + 1)
+
+    def _update_backstage_pass(self, item):
+        # Drops to 0 after concert
+        # +3 when <= 5 days, +2 when <= 10 days, +1 otherwise
+        if item.sell_in < 0:
+            item.quality = 0
+        elif item.sell_in < 5:
+            item.quality = min(MAX_QUALITY, item.quality + 3)
+        elif item.sell_in < 10:
+            item.quality = min(MAX_QUALITY, item.quality + 2)
+        else:
+            item.quality = min(MAX_QUALITY, item.quality + 1)
 
 
 class Item:
